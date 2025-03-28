bb7f08ce75a05a5784d8214b744fb15106f2e63d
Tenshi . <jrcontreras446@gmail.com>
2025-03-28 17:22:26 -0600

    added all logic, not finish though

---
+++ README.txt
@@ -0,0 +1,2 @@
+No logre encontrar el error que me estaba dando el error, entonces decidi ir copiando el codigo del profesor.
+/back-up es hasta donde yo llegue, el resto fue lo que decidi copiar.
--- js/main.js
+++ js/main.js
@@ -1,40 +1,70 @@
-const resultado = document.querySelector('#resultado');
-const formulario = document.querySelector('#formulario');
 
-window.onload =()=>{ 
-  formulario.addEventListener('submit', validarFormulario);
+const resultado = document.querySelector( "#resultado")
+const formulario = document.querySelector("#formulario")
+
+
+window.onload = () => {
+    formulario.addEventListener('submit', validarFormulario);
 }
 
-function validarFormulario(e){
-  e.preventDefault();
-
-  const terminoBusqueda = document.querySelector('#termino').value.trim();
-
-  if (terminoBusqueda === ''){
-    mostrarAlerta('Error, termino de busqueda no agregado');
-    return;
-  }
+function validarFormulario(e) {
+    e.preventDefault();
+    const terminoBusqueda = document.querySelector('#termino').value.trim();
+    if (terminoBusqueda === '') {
+        mostrarAlerta('Error, no ha agregado un término de búsqueda');
+        return; 
+    }
+    buscarImagenes(terminoBusqueda);
 }
 
-function mostrarAlerta(mensaje) {
-  const existeAlerta = document.querySelector('.alerta');
 
-  if (!existeAlerta) {
-    const alerta = document.createElement('p');
-    alerta.className = 'alerta';
-    alerta.innerHTML = mensaje;
-    formulario.appendChild(alerta);
-
-    setTimeout(() => {
-      alerta.remove();
-    }, 3000);
-  }
+function mostrarAlerta(mensaje){
+    const existeAlerta = document.querySelector(".alerta");
+    if(!existeAlerta){
+        const alerta = document.createElement('p');
+        alerta.className = "alerta";
+        alerta.innerHTML = mensaje;
+        formulario.appendChild(alerta);   
+        
+        setTimeout(() =>{
+            alerta.remove();
+        }, 3000);
+    }
 }
-
 
 function buscarImagenes(termino){
-  const url = 'https://pixabay.com/api/?key=49547329-5f5a917eedf2fb607ff48be58&q=' + termino;
-  console.log(url);
+    const key = '49389642-ac93a7b768c33d60ce2c87622';
+    const url = 'https://pixabay.com/api/?key=49389642-ac93a7b768c33d60ce2c87622&q=' + termino;
+   /* const url = 'https://pixabay.com/api/?key=49389642-ac93a7b768c33d60ce2c87622&q=' + termino + '&per_page=100';*/
+    //console.log(url);
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => {
+            //console.log(resultado);
+            mostrarImagenes(resultado.hits)
+        })
 }
 
+function mostrarImagenes(imagenes){
+    //console.log(imagenes); 
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
 
+    imagenes.forEach(imagen => {
+        const {previewURL, likes, views, largeImageURL} = imagen; //
+        resultado.innerHTML  += `
+        <div class = "contenido">
+            <div class = "tarjeta">
+                 <img src="${previewURL}">
+                 <div class = "info-tarjeta">
+                    <p class = "likes">${likes} <span>Me gusta</span></p>
+                    <p class = "views">${views} <span>Me gusta</span></p>
+                    <a href = "${largeImageURL}" target = "_blank">Ver Imagen</a>
+                 </div>
+            </div>
+        </div>    
+        `
+    });
+}
